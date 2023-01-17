060fab8360d177664a0ba003462608e7aef3a677
EC2 Default User <ec2-user@ip-172-31-0-152.ap-northeast-1.compute.internal>
2023-01-17 07:25:20 +0000

    [create miniature]nagano-cake

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,16 @@
+class Public::CustomersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def withdrawal
+  end
+
+  def status
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,60 +1,56 @@
 Rails.application.routes.draw do
+
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+  }
+
+  # 顧客用
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+  # admin
+
   namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
+    get 'homes/top' => "homes#top", as: "top"
+
+    resources :customers, only: [:index, :show, :edit, :update]
+
+    resources :orders, only: [:show, :update]
+
+    resources :order_details, only: [:update]
+
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+
+    resources :genres, only: [:create, :index, :edit, :update]
   end
-  get 'customers/show'
-  get 'customers/edit'
-  get 'customers/update'
-  get 'customers/withdrawal'
-  get 'customers/status'
-  namespace :admin do
-    get 'orders/update'
+
+  # public
+
+  scope module: :public do
+
+    root to: 'homes#top'
+
+    get 'homes/about' => "homes#about", as: "about"
+
+    resources :customers, only: [:show, :edit, :update]
+    get 'customers/withdrawal' => "customers#withdrawal", as: "withdrawal"
+    get 'customers/status' => "customers#status", as: "status"
+
+    resources :addresses, only: [:create, :edit, :update, :index, :destroy]
+
+    resources :orders, only: [:new, :create, :show, :index]
+    get 'orders/order_check' => "orders#order_check", as: "order_check"
+    get 'orders/order_finish' => "orders#order_finish", as: "order_finish"
+
+    resources :cart_items, only: [:create, :index, :update, :destroy]
+    get 'cart_items/destroy_all' => "cart_items#destroy_all", as: "destroy_all"
+
+    resources :items, only: [:show, :index]
+
   end
-  namespace :adimin do
-    get 'order_details/show'
-    get 'order_details/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-    get 'genres/create'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  get 'addresses/index'
-  get 'addresses/edit'
-  get 'addresses/create'
-  get 'addresses/update'
-  get 'addresses/destroy'
-  get 'orders/new'
-  get 'orders/order_check'
-  get 'orders/order_finish'
-  get 'orders/create'
-  get 'orders/index'
-  get 'orders/show'
-  get 'cart_items/index'
-  get 'cart_items/update'
-  get 'cart_items/destroy'
-  get 'cart_items/destroy_all'
-  get 'cart_items/create'
-  get 'items/index'
-  get 'items/show'
-  get 'homes/top'
-  get 'homes/about'
-  devise_for :admins
-  devise_for :customers
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
