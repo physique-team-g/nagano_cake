62a24d1ac0a039864836d422ad0f296238387fc7
EC2 Default User <ec2-user@ip-172-31-0-152.ap-northeast-1.compute.internal>
2023-01-20 09:57:01 +0000

    [update address]ながのcake

--- config/routes.rb
+++ config/routes.rb
@@ -50,8 +50,6 @@ Rails.application.routes.draw do
 
     resources :items, only: [:show, :index]
 
-    resources :genres, only: [:create, :index, :edit, :update]
-
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
