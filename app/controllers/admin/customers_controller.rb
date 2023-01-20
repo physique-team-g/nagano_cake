3bd457616c5e2f84b5cf4e16327c1e9a9b66134c
EC2 Default User <koichan1996@icloud.com>
2023-01-20 05:33:21 +0000

    admin/customer_controller

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,8 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
+    @customer = Customer.find(params[:id])
+    # @customer = current_customer
   end
 
   def show
