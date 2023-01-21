f89f68ff0072d98e5fe63ca65b85a07105ad5486
EC2 Default User <koichan1996@icloud.com>
2023-01-21 05:02:21 +0000

    /admin/customers/id

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -21,4 +21,11 @@ class Admin::CustomersController < ApplicationController
       render "edit"
     end
   end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email)
+  end
+  
 end
