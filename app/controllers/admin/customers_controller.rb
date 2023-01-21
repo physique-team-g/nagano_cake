fb2d87582ff7ba8268435472d684a4991e52091f
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-01-21 09:25:30 +0000

    [add]admin_item

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -17,13 +17,13 @@ class Admin::CustomersController < ApplicationController
     @customer.update(customer_params)
       redirect_to admin_customer_path(@customer)
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email)
   end
-  
+
 end
 
 private
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,12 +14,18 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+    @total_price = @item.price * 1.08
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item)
   end
 end
 
