4f9368f5654dd8e6e153d9d500e36c304dc86832
EC2 Default User <ec2-user@ip-172-31-0-152.ap-northeast-1.compute.internal>
2023-01-21 10:46:30 +0000

    [update rink]ながのcake

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,19 +1,21 @@
 class Public::CustomersController < ApplicationController
   def show
-    @customer = Customer.find(params[:id])
+    @customer = current_customer
+
   end
 
   def edit
-    @customer = Customer.find(params[:id])
+    @customer = current_customer
   end
 
   def update
-    @customer = Customer.find(params[:id])
+    @customer = current_customer
     @customer.update(customer_params)
-    redirect_to customer_path(@customer.id)
+    redirect_to customers_path
   end
 
   def withdrawal
+    @customer = current_customer
   end
 
   def status
