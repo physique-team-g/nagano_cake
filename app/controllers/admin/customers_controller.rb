7ebf16301109851e125ba1739b3cdef418e3d674
EC2 Default User <ec2-user@ip-172-31-36-110.ap-northeast-1.compute.internal>
2023-01-21 07:56:20 +0000

    test

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,8 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.all
-    @customer = Customer.find(params[:id])
-    # @customer = current_customer
   end
 
   def show
@@ -21,5 +19,5 @@ class Admin::CustomersController < ApplicationController
       render "edit"
     end
   end
-  
+
 end
