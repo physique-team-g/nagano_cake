c09fafce8fad761245ec0844c88c8dcffa6a6321
EC2 Default User <ec2-user@ip-172-31-0-152.ap-northeast-1.compute.internal>
2023-01-22 11:20:18 +0000

    [update cart_item index]ながのcake

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -16,17 +16,15 @@ class Public::CartItemsController < ApplicationController
     @cart_item = current_customer.cart_items.new(cart_item_params)
      if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
         cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-
-        cart_item.quantity += params[:cart_item][:amount].to_i
-
+        cart_item.amount += params[:cart_item][:amount].to_i
         cart_item.save
         redirect_to cart_items_path
      elsif
         @cart_item.save
         @cart_items = current_customer.cart_items.all
-        rander 'index'
+        render 'index'
      else
-        rander 'index'
+        render 'index'
      end
    end
 
@@ -34,5 +32,5 @@ class Public::CartItemsController < ApplicationController
     def cart_item_params
         params.require(:cart_item).permit(:item_id, :price, :amount, :customer_id)
     end
-    
+
 end
