5082ad608ae7be8d768e61f30d8cdf137fcc93fd
EC2 Default User <ec2-user@ip-172-31-36-110.ap-northeast-1.compute.internal>
2023-01-22 10:21:29 +0000

    public_items_show_fixed

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,18 +5,18 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @cart_item = CartItem
+    @cart_item = CartItem.new
   end
-  
-  
+
+
   private
-  
+
   def item_params
     params.require(:item).permit(:genre_id,:name,:introduction,:item_image,:price)
   end
-  
+
   private
-  
+
   def item_params
     params.require(:item).permit(:genre_id, :introduction, :item_image, :price)
   end
