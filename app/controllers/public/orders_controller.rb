3dd4c906a68e2454194fec1536aec6dc18cce1ab
EC2 Default User <ec2-user@ip-172-31-0-152.ap-northeast-1.compute.internal>
2023-01-23 10:07:58 +0000

    [update order_controller]ながのcake

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,7 +5,27 @@ class Public::OrdersController < ApplicationController
 
   def order_check
     @order_check = current_customer.cart_items.all
-    @order = Order.all
+    @order = Order.new(order_params)
+    if params[:order][:address_number] == "1"
+       @order.name = current_customer.name
+       @order.address = current_customer.customer_address
+    elsif
+       params[:order][:address_number] == "2"
+      if Address.exists?(name: params[:order][:registered])
+         @order.name = Address.find(params[:order][:registered]).name
+         @order.address = Address.find(params[:order][:registered]).address
+      else
+      render :new
+      end
+    elsif params[:order][:address_number] == "3"
+      address_new = current_customer.addresses.new(address_params)
+      if address_new.save
+      else
+         render :new
+      end
+    else
+      redirect_to 遷移したいページ
+    end
   end
 
   def order_finish
@@ -20,4 +40,12 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+end
+
+def order_params
+  params.require(:order).permit(:name, :address, :total_price)
+end
+
+def address_params
+  params.require(:order).permit(:name, :address)
 end
