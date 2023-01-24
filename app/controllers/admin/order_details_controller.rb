8d6e9a468fa327cba8a4191d8579d9fe275a37a5
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-01-24 11:50:59 +0000

    [add]admin/orders

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,12 @@
 class Adimin::OrderDetailsController < ApplicationController
   def update
+    order_details = OrderDetail.find(params[:id])
+    order_details.update(order_details_params)
+    redirect_to admin_order_path(order)
   end
+
+  private
+   def order_details_params
+    params.require(:order_details).permit(:product_status)
+   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,18 +1,29 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @order_details =@order.order_details.all
   end
 
   def update
-    order = Order.find(params[:id])
+    @order = Order.find(params[:id])
     order.update(order_params)
+    redirect_to admin_order_path(order)
+  end
+
+  def update_status
+    @order = Order.find(params[:id])
+    order_details = @order.order_details.find(params[:id])
+    order_details.update(order_details_params)
     redirect_to admin_order_path(order)
   end
 
 
 private
   def order_params
-    params.require(:order, :order_details).permit(:status, :product_status)
+    params.require(:order).permit(:status)
   end
 
+  def order_details_params
+    params.require(:order_details).permit(:product_status)
+  end
 end
