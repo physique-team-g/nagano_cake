9955a8eedef4afa1371bb0a2a3c949d4754a1f52
EC2 Default User <ec2-user@ip-172-31-0-152.ap-northeast-1.compute.internal>
2023-01-25 12:05:46 +0000

    [no change]ながのcake

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,7 +2,7 @@ class Admin::HomesController < ApplicationController
 before_action :authenticate_admin!
 
   def top
-     @orders = current_customer.orders.all.page(params[:page]).per(10)
+     @orders = Order.all.page(params[:page]).per(10)
   end
 
 end
