a6565149292c19214e607934912510d65a73a201
EC2 Default User <ec2-user@ip-172-31-36-110.ap-northeast-1.compute.internal>
2023-01-26 08:50:59 +0000

    [public_cartitems_index_fixed]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,21 +1,24 @@
 class Admin::OrdersController < ApplicationController
-before_action :authenticate_admin!
-
   def show
     @order = Order.find(params[:id])
     @order_details =@order.order_details.all
-    @order_detail
   end
 
   def update
-    order = Order.find(params[:id])
+    @order = Order.find(params[:id])
     order.update(order_params)
     redirect_to admin_order_path(order)
   end
 
+  def update_status
+    @order = Order.find(params[:id])
+    order_details = @order.order_details.find(params[:id])
+    order_details.update(order_details_params)
+    redirect_to admin_order_path(order)
+  end
 
-  private
 
+private
   def order_params
     params.require(:order).permit(:status)
   end
@@ -23,5 +26,4 @@ before_action :authenticate_admin!
   def order_details_params
     params.require(:order_details).permit(:product_status)
   end
-
 end
