44985dbf31c6727bbf55fbe7595eb81ced129ef4
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-01-26 08:29:51 +0000

    [fixed]update_status

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,9 +2,21 @@ class Admin::OrderDetailsController < ApplicationController
 before_action :authenticate_admin!
 
   def update
-    order_details = OrderDetail.find(params[:id])
-    order_details.update(order_details_params)
-    redirect_to admin_order_path(order_details.order.id)
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_details = @order.order_details.all
+    @order_detail.update(order_details_params)
+
+    if @order_details.where(product_status: 2).count >= 1
+      @order.status = 2
+      @order.save
+    end
+
+    if @order_details.where(product_status: 3).count == @order_details.count
+      @order.status = 3
+      @order.save
+    end
+    redirect_to admin_order_path(@order)
   end
 
 
